17a066d3ca63f1f608a54d8e92864b16f4da2b97
Phan Trung Thuận <143245984+Phan-Trung-Thuan@users.noreply.github.com>
2026-03-21 20:04:20 +0700

    Update infer.sh

--- infer/infer.sh
+++ infer/infer.sh
@@ -3,7 +3,7 @@
 # TODO: change the yaml, text list, ckpt path
 
 # for better music quality
-python3 infer_mos5.py \
+python3 infer/infer_mos5.py \
 --config_yaml audioldm_train/config/mos_as_token/qa_mdt.yaml \
 --list_inference prompts/good_prompts_1.lst \
 --reload_from_ckpt "output/model_checkpoint.ckpt"
