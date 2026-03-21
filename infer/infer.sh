580846a08109698034aac9707b4df952afa65c95
Phan Trung Thuận <143245984+Phan-Trung-Thuan@users.noreply.github.com>
2026-03-21 20:25:41 +0700

    Update infer.sh

--- infer/infer.sh
+++ infer/infer.sh
@@ -5,7 +5,7 @@
 # for better music quality
 python3 infer/infer_mos5.py \
 --config_yaml audioldm_train/config/mos_as_token/qa_mdt.yaml \
---list_inference prompts/good_prompts_1.lst \
+--list_inference test_prompts/good_prompts_1.lst \
 --reload_from_ckpt "output/model_checkpoint.ckpt"
 
 # for better overrall quality
